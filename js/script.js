5cbadef2ea32ec19fd4b2c6d0643e5c6c61ee4eb
V <36138541+artnerdnet@users.noreply.github.com>
2018-07-22 16:51:32 +0200

    Update script.js
    Added error message when username is undefined

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const username = document.getElementById('usernane');
 const reposContainer = document.getElementById('reposView');
 
 searchForm.addEventListener('submit', function(e) {
-    userView.innerHTML = '';
     searchedTerm = searchField.value;
 
     fetch(`https://api.github.com/users/${searchedTerm}`, {
@@ -15,104 +14,114 @@ searchForm.addEventListener('submit', function(e) {
         },
         mode: 'cors'
     }).then(response => response.json()).then(function(data) {
-
-        let userFullName = data.name;
         let userLogin = data.login;
+        let userFullName = data.name;
         let userBio = data.bio;
         let userAvatar = data.avatar_url;
 
-        let addUserAvatar = () => {
-            let imageContainer = document.getElementById('userImg');
-            imageContainer.src = userAvatar;
-        }
+        if (userLogin == undefined) {
+            let errorMessage = document.getElementById('errorMsg');
+            errorMessage.classList.toggle('hide');
+        } else {
+            userView.innerHTML = '';
 
-        let addUsernameInfo = () => {
-            let usernameSpan = document.createElement('span');
-            usernameSpan.id = 'username';
-            usernameSpan.innerHTML += `@ ${userLogin}`;
-            userContainer.appendChild(usernameSpan);
-        }
-
-
-        let addUserFullNameInfo = () => {
-            let userFullNameSpan = document.createElement('h1');
-            userFullNameSpan.id = 'fullname';
-            userFullNameSpan.innerHTML += userFullName;
-            userContainer.appendChild(userFullNameSpan);
-        }
-
-        let addUserBio = () => {
-            let userBioDiv = document.createElement('div');
-            userBioDiv.id = 'bioBox';
-            userBioDiv.innerHTML += `<span id='biography'>${userBio}</span>`;
-            userContainer.appendChild(userBioDiv);
-        }
-
-        addUserAvatar();
-        addUsernameInfo();
-        addUserFullNameInfo();
-        addUserBio();
-
-    });
-
-
-    fetch(`https://api.github.com/users/${searchedTerm}/repos`, {
-        headers: {
-            Authorization: 'Client-ID 583f4cd67ae6eb74a55f'
-        },
-        mode: 'cors'
-    }).then(response => response.json()).then(function(data) {
-        let repoListing = document.getElementById('repoList');
-        repoListing.innerHTML = '';
-
-        for (info of data) {
-            let repoName = info.name;
-            let repoURL = info.html_url;
-            let repoForks = info.forks_count;
-            let repoStars = info.stargazers_count;
-
-            let createLi = document.createElement('li');
-            repoListing.appendChild(createLi);
-            createLi.classList.add('repoListElement');
-
-            let addRepoTitle = () => {
-                let repoTitleSpan = document.createElement('span');
-                repoTitleSpan.innerHTML += repoName.link(repoURL);
-                createLi.appendChild(repoTitleSpan);
-                repoTitleSpan.classList.add('repoTitle');
+            let addUserAvatar = () => {
+                let imageContainer = document.getElementById('userImg');
+                imageContainer.src = userAvatar;
             }
 
-            let addForkCount = () => {
-                let repoForkCountSpan = document.createElement('span');
-                repoForkCountSpan.innerHTML += repoForks;
-                createLi.appendChild(repoForkCountSpan);
-                repoForkCountSpan.classList.add('repoForkCount');
+            let addUsernameInfo = () => {
+                let usernameSpan = document.createElement('span');
+                usernameSpan.id = 'username';
+                usernameSpan.innerHTML += `@ ${userLogin}`;
+                userContainer.appendChild(usernameSpan);
             }
 
-            let addStarCount = () => {
-                let repoStarCountSpan = document.createElement('span');
-                repoStarCountSpan.innerHTML += repoStars;
-                createLi.appendChild(repoStarCountSpan);
-                repoStarCountSpan.classList.add('repoStarCount');
+
+            let addUserFullNameInfo = () => {
+                let userFullNameSpan = document.createElement('h1');
+                userFullNameSpan.id = 'fullname';
+                userFullNameSpan.innerHTML += userFullName;
+                userContainer.appendChild(userFullNameSpan);
             }
 
-            let addStarIcon = () => {
-                let repoStarIconSpan = document.createElement('span');
-                createLi.appendChild(repoStarIconSpan);
-                repoStarIconSpan.classList.add('fas', 'fa-star');
+            let addUserBio = () => {
+                let userBioDiv = document.createElement('div');
+                userBioDiv.id = 'bioBox';
+                userBioDiv.classList.add('scrollBar');
+                userBioDiv.innerHTML += `<span id='biography'>${userBio}</span>`;
+                userContainer.appendChild(userBioDiv);
             }
 
-            let addForkIcon = () => {
-                let repoForkIconSpan = document.createElement('span');
-                createLi.appendChild(repoForkIconSpan);
-                repoForkIconSpan.classList.add('fas', 'fa-code-branch');
-            }
-
-            addRepoTitle();
-            addStarIcon();
-            addStarCount();
-            addForkIcon();
-            addForkCount();
+            addUserAvatar();
+            addUsernameInfo();
+            addUserFullNameInfo();
+            addUserBio();
+            fetchRepos();
         }
     })
+
+    let fetchRepos = () => {
+        fetch(`https://api.github.com/users/${searchedTerm}/repos`, {
+            headers: {
+                Authorization: 'Client-ID 583f4cd67ae6eb74a55f'
+            },
+            mode: 'cors'
+
+        }).then(response => response.json()).then(function(data) {
+            let repoOwner = data.owner;
+            let repoListing = document.getElementById('repoList');
+            repoListing.innerHTML = '';
+
+            for (info of data) {
+                let repoName = info.name;
+                let repoURL = info.html_url;
+                let repoForks = info.forks_count;
+                let repoStars = info.stargazers_count;
+
+                let createLi = document.createElement('li');
+                repoListing.appendChild(createLi);
+                createLi.classList.add('repoListElement');
+
+                let addRepoTitle = () => {
+                    let repoTitleSpan = document.createElement('span');
+                    repoTitleSpan.innerHTML += repoName.link(repoURL);
+                    createLi.appendChild(repoTitleSpan);
+                    repoTitleSpan.classList.add('repoTitle');
+                }
+
+                let addForkCount = () => {
+                    let repoForkCountSpan = document.createElement('span');
+                    repoForkCountSpan.innerHTML += repoForks;
+                    createLi.appendChild(repoForkCountSpan);
+                    repoForkCountSpan.classList.add('repoForkCount');
+                }
+
+                let addStarCount = () => {
+                    let repoStarCountSpan = document.createElement('span');
+                    repoStarCountSpan.innerHTML += repoStars;
+                    createLi.appendChild(repoStarCountSpan);
+                    repoStarCountSpan.classList.add('repoStarCount');
+                }
+
+                let addStarIcon = () => {
+                    let repoStarIconSpan = document.createElement('span');
+                    createLi.appendChild(repoStarIconSpan);
+                    repoStarIconSpan.classList.add('fas', 'fa-star');
+                }
+
+                let addForkIcon = () => {
+                    let repoForkIconSpan = document.createElement('span');
+                    createLi.appendChild(repoForkIconSpan);
+                    repoForkIconSpan.classList.add('fas', 'fa-code-branch');
+                }
+
+                addRepoTitle();
+                addStarIcon();
+                addStarCount();
+                addForkIcon();
+                addForkCount();
+            }
+        })
+    }
 });
