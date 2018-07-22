bd439c1e33d653223af8d3bf9d85ea458f2a049d
V <36138541+artnerdnet@users.noreply.github.com>
2018-07-22 15:04:18 +0200

    Update script.js
    Refactorized user info into arrow functions

--- js/script.js
+++ js/script.js
@@ -8,55 +8,87 @@ const reposContainer = document.getElementById('reposView');
 searchForm.addEventListener('submit', function(e) {
     userView.innerHTML = '';
     searchedTerm = searchField.value;
-    
+
     fetch(`https://api.github.com/users/${searchedTerm}`, {
         headers: {
-            Authorization: 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3'
+            Authorization: 'Client-ID 583f4cd67ae6eb74a55f'
         },
         mode: 'cors'
     }).then(response => response.json()).then(function(data) {
-        let addUserInfo = () => {
-            let userFullName = data.name;
-            let userLogin = data.login;
-            let userBio = data.bio;
-            userContainer.innerHTML = `<span id='username'>@${userLogin}</span><h1 id='fullname'>${userFullName}</h1><div id='bioBox' class='scrollBar'><span id='biography'>${userBio}</span></div>`;
+
+        let userFullName = data.name;
+        let userLogin = data.login;
+        let userBio = data.bio;
+        let userAvatar = data.avatar_url;
+
+        let addUserAvatar = () => {
+            let imageContainer = document.getElementById('userImg');
+            imageContainer.src = userAvatar;
         }
-        addUserInfo();
+
+        let addUsernameInfo = () => {
+            let usernameSpan = document.createElement('span');
+            usernameSpan.id = 'username';
+            usernameSpan.innerHTML += `@ ${userLogin}`;
+            userContainer.appendChild(usernameSpan);
+        }
+
+
+        let addUserFullNameInfo = () => {
+            let userFullNameSpan = document.createElement('h1');
+            userFullNameSpan.id = 'fullname';
+            userFullNameSpan.innerHTML += userFullName;
+            userContainer.appendChild(userFullNameSpan);
+        }
+
+        let addUserBio = () => {
+            let userBioDiv = document.createElement('div');
+            userBioDiv.id = 'bioBox';
+            userBioDiv.innerHTML += `<span id='biography'>${userBio}</span>`;
+            userContainer.appendChild(userBioDiv);
+        }
+
+        addUserAvatar();
+        addUsernameInfo();
+        addUserFullNameInfo();
+        addUserBio();
+
     });
+
 
     fetch(`https://api.github.com/users/${searchedTerm}/repos`, {
         headers: {
-            Authorization: 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3'
+            Authorization: 'Client-ID 583f4cd67ae6eb74a55f'
         },
         mode: 'cors'
     }).then(response => response.json()).then(function(data) {
         let repoListing = document.getElementById('repoList');
         repoListing.innerHTML = '';
-        
+
         for (info of data) {
             let repoName = info.name;
             let repoURL = info.html_url;
             let repoForks = info.forks_count;
             let repoStars = info.stargazers_count;
-            
+
             let createLi = document.createElement('li');
             repoListing.appendChild(createLi);
-            createLi.classList.add('repoListElement');        
-            
+            createLi.classList.add('repoListElement');
+
             let addRepoTitle = () => {
                 let repoTitleSpan = document.createElement('span');
                 repoTitleSpan.innerHTML += repoName.link(repoURL);
                 createLi.appendChild(repoTitleSpan);
                 repoTitleSpan.classList.add('repoTitle');
-            }           
-            
+            }
+
             let addForkCount = () => {
                 let repoForkCountSpan = document.createElement('span');
                 repoForkCountSpan.innerHTML += repoForks;
                 createLi.appendChild(repoForkCountSpan);
                 repoForkCountSpan.classList.add('repoForkCount');
-            }            
-            
+            }
+
             let addStarCount = () => {
                 let repoStarCountSpan = document.createElement('span');
                 repoStarCountSpan.innerHTML += repoStars;
@@ -69,7 +101,7 @@ searchForm.addEventListener('submit', function(e) {
                 createLi.appendChild(repoStarIconSpan);
                 repoStarIconSpan.classList.add('fas', 'fa-star');
             }
-            
+
             let addForkIcon = () => {
                 let repoForkIconSpan = document.createElement('span');
                 createLi.appendChild(repoForkIconSpan);
